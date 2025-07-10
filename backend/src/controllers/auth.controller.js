053f660e2ce562e6b816bbb5dfa75517a3c0d268
reliable99 <abdulmuhminsodiq@gmail.com>
2025-07-10 06:36:55 +0100

    connecting dD

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+
+export const Signup  = (req, res) => {
+    res.send("signup route")
+}
+export const Login  = (req, res) => {
+    res.send("login route")
+}
+export const Logout  = (req, res) => {
+    res.send("logout route")
+}
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.route.js"
+import { connectDb } from "./Lib/lib.js";
+
+dotenv.config()
+
+const app = express();
+
+app.use("/api/auth", authRoutes)
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log("server is running on PORT:" + PORT)
+    connectDb()
+})
