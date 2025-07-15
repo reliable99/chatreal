c04af9dac3a7e33b64524b24e33e9ead33da6909
reliable99 <abdulmuhminsodiq@gmail.com>
2025-07-15 20:03:41 +0100

    signup-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,18 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 
 export default function App() {
-  const {authUser,checkAuth} = useAuthStore()
+  const {authUser,checkAuth, isCheckingAuth} = useAuthStore()
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth])
   console.log({authUser})
+
+  if(isCheckingAuth &&  !authUser ) return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
   return (
     <div>
       <Navbar />
